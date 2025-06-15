d1247e0a405f8ed662902c6d66f80838b2863df2
DiegoTalledoS18 <diegotasa35@hotmail.com>
2025-06-14 21:08:21 -0500

    add: launcher icon

--- lib/ui/pages/profile/profile_screen.dart
+++ lib/ui/pages/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../authentication/login_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,27 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: IconButton(
+              icon: const Icon(Icons.logout, color: Colors.black),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                      (route) => false, // Elimina todas las rutas anteriores
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Profile Screen'),
+      ),
+    );
   }
 }
